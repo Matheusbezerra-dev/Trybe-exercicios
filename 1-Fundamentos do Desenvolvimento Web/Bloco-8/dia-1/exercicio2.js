6e437052a17082a8f4f25470cfbd5a55ade54a1d
Matheus Bezerra <mabezerra2@gmail.com>
2022-06-22 19:24:14 -0400

    finalizando o exercicio 2

---
+++ 1-Fundamentos do Desenvolvimento Web/Bloco-8/dia-1/exercicio2.js
@@ -0,0 +1,7 @@
+const numberChecker = (myNumber, number) => myNumber === number;
+const lotteryResult = (myNumber, callBack) => {
+  const numberOne = Math.floor((Math.random() * 5) + 1);
+  return callBack(myNumber, numberOne) ? 'Lucky day, you won!' : 'Try Again!';
+};
+
+console.log(lotteryResult(5, numberChecker));
