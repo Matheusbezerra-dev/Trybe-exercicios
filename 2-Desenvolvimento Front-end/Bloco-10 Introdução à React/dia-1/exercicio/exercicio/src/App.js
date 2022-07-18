68e5ab600376804edb98d2150a5f05a8d005e15b
Matheus Bezerra <mabezerra2@gmail.com>
2022-07-18 19:32:13 -0400

    finalizando o exercicios

--- 2-Desenvolvimento Front-end/Bloco-10 Introdução à React/dia-1/exercicio/exercicio/src/App.js
+++ 2-Desenvolvimento Front-end/Bloco-10 Introdução à React/dia-1/exercicio/exercicio/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ const Tasks = ['acorda', 'toma banho', 'tomar café da manhã', 'estudar', 'estudar mais'];
+ const result =  Tasks.map((Task) => <li> {Task} </li>);
+
+class Task extends Component {
+  render() {   
+    return (
+        <div>
+          <ul>
+            { result }
+          </ul>
+        </div>
+    ); 
+  };
 }
 
-export default App;
+
+export default Task;
